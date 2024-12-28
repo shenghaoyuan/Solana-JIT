4e9bab5f645e93dbdb7340d6d907022ccbdb580b
shenghaoyuan <shenghaoyuan0928@163.com>
2024-12-28 10:53:08 +0800

    add: world

--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# Solana-JIT
+# A complete formal semantics of eBPF instruction set architecture for Solana
+
+Note that we only test our project on
+- Windows 11 + WSL2 (Ubuntu 22.04 LTS)
+- Ubuntu 22.04 LTS
+
+plus `CPU: Intel(R) Core(TM) Ultra 7 155H   1.40 GHz` + `RAM 32G` + `Core: 16`
+
+_We record how we install all necessary packages on a fresh Ubuntu22.04 environment._ see `intall_log.md`
+# 1. SBPF ISA Semantics
+## 1.1 Install Isabelle/HOL and AFP
+- [Isabelle/HOL 2024](https://isabelle.in.tum.de/) (please set your PATH with e.g. `/YOUR-PATH/Isabelle2024`)
+
+- [Isabelle AFP](https://www.isa-afp.org/download/) (unzip the AFP to your PATH, e.g. `/YOUR-PATH/afp`)
+
+```shell
+# set PATH 
+vim  ~/.bashrc # export PATH=$PATH:/YOUR-PATH/Isabelle2024/bin:...
+source ~/.bashrc
+
+# test isabelle/hol
+isabelle version # Isabelle2024
+
+# config AFP
+cd /YOUR-PATH/afp/thys
+isabelle components -u . # Add AFP to ...
+
+# go to our repo folder and open this project in jedit
+cd /OUR-REPO
+
+# adding the following libs when install on WSL2 with Ubuntu 22.04.3 LTS (GNU/Linux 5.15.153.1-microsoft-standard-WSL2 x86_64)
+sudo apt-get install libxi6 libxtst6 libxrender1 fontconfig
+```
+
+## 1.2 Check the SBPF ISA semantics
+This command starts up the IDE of Isabelle/HOL (JEdit), opens the `Interpreter.thy` file, and checks the semantics automatically.
+```shell
+make
+```
+If you want to check another file, just click it on JEdit and the Isabelle/HOL checker then validates it automatically.
+
+## 1.3 Link to paper
+
+| Paper      | Code      |
+| ------------- | ------------- |
+| Syntax (Section 4.1, Fig 4) | `theory/rBPFSyntax.thy#L41` |
+| Semantics (Section 4.2) | `theory/Interpreter.thy#L510`, `theory/Interpreter.thy#L608` |
+
+# 2. Validation Framework
+
+## 2.1 Install Rust, OCaml and related packages
+
+First, check the Official [Rust web](https://www.rust-lang.org/tools/install) to install Rust.
+The following instructions explains how to install OCaml + packages
+```shell
+# install opam
+
+sudo apt install opam
+# In the case you fail to install opam
+# Note1: you may need the two commands before install opam, i.e. `add-apt-repository ppa:avsm/ppa` and `apt update`
+# Note2: you may need to change your source list to focal source if `add-apt-repository ppa:avsm/ppa` fails
+
+
+# install ocaml+coq by opam
+opam init
+# install ocaml
+opam switch create sbpf ocaml-base-compiler.4.14.1
+
+eval $(opam env)
+
+opam switch list
+#   switch  compiler      description
+->  sbpf     ocaml.4.14.1  sbpf
+
+# Note3: Once you get a warning here, please do `eval $(opam env)`, restart your computer/VM, and do `eval $(opam env)` again
+
+# make sure your ocaml is 4.14.1 and from `/home/bob/.opam/sbpf/bin/ocaml`
+which ocaml
+
+# install necessary packages
+opam install ocamlfind yojson
+```
+
+## 2.2 Validate semantics
+- **`make macro-test`**: Compiles and runs program-level tests using the Solana official test suite in `tests/rbpf/tests/execution.rs`.
+- **`make micro-test`**: Compiles and runs instruction-level tests using the generated cases (100 tests by default).
+
+We also provide **`make generator num=X`** to generate X random instruction test cases. For example, to do 100000 micro-test, just run `make generator num=100000; make micro-test`
+
+## 2.3 Link to paper
+
+| Paper      | Code      |
+| ------------- | ------------- |
+| Validation Framework (Section 5) | isabell/hol: glue code1 `theory/Interpreter.thy#L651` + glue code2 `theory/Interpreter.thy#L683` + extraction declration `theory/bpf_generator.thy#L15`, OCaml: glue code `tests/exec_semantics/glue.ml`, interpreter_test `tests/exec_semantics/interp_test.ml`, step_test `tests/exec_semantics/step_test.ml` |
+
+# 3. Solana VM applications
+
+## 3.1 Solana Assembler and Disassembler (Section 6.1)
+
+| Paper      | Code      |
+| ------------- | ------------- |
+| Solana Assembler | `theory/Assembler.thy#L227` |
+| Solana Disassembler | `theory/Disassembler.thy#L515` |
+| Consistency Proof (Theorem 6.3) | `theory/ConsistencyProof.thy#L8` |
+
+
+## 3.2 Solana Verifier (Section 6.2)
+
+| Paper      | Code      |
+| ------------- | ------------- |
+| Solana Verifier | `theory/verifier.thy#L235` |
+| Solana Verifier Proof (Lemma 6.4) | `theory/VerifierSafety.thy#L13` |
+
+## 3.3 Solana x64 JIT Compiler (Section 6.3)
+
+| Paper      | Code      |
+| ------------- | ------------- |
+| x64 model | `theory/x64Semantics.thy` |
+| x64 equivalence proof | `theory/x64DecodeProof.thy#L11`: has sufficiently provided the infrastructure for proving the Solana JIT correctness |
+| Solana JIT | `theory/JITCommType.thy#L264` |
+| Solana JIT Proof | `theory/bpfConsistencyAux.thy`, `theory/bpfConsistencyAux1.thy`, `theory/bpfConsistencyAux2.thy`, `theory/bpfConsistencyAux3.thy` |
+
+
+# 4. Code Statistics (Section 7.1)
+## 4.1 Install the `cloc` tool
+```shell
+sudo apt-get install cloc
+``` 
+## 4.2. Run the following command to get the lines of code 
+Note that currently `cloc` doesn't support Isabelle/HOL now, we specify the lanuage to OCaml because both use `(* *)` as comments.
+```shell
+make code
+```
---
+++ theory/JIT_abs.thy
@@ -0,0 +1,375 @@
+section \<open> A collection of common type of JIT \<close>
+
+theory JIT_abs
+imports
+  Main
+  rBPFCommType rBPFSyntax
+  vm x86CommType Interpreter x64Semantics x64Disassembler  x64Assembler
+  StepSem
+begin
+
+record JitProgram =
+page_size     :: usize
+pc_section    :: "usize list"
+text_section  :: "x64_bin"
+
+record jit_state =
+jit_result :: JitProgram 
+offset_in_text_section :: usize 
+jit_pc :: usize 
+
+definition update_pc_section_aux ::"usize list \<Rightarrow> nat \<Rightarrow> usize \<Rightarrow> usize list" where
+"update_pc_section_aux pc_sec pc x = list_update pc_sec pc x"
+
+(*
+definition update_pc_section ::"JitCompiler \<Rightarrow> JitCompiler" where
+"update_pc_section jcomp = (let jprog = jit_result jcomp;
+   r = pc_section jprog; x = update_pc_section_aux r (unat (jit_pc jcomp)) (offset_in_text_section jcomp) in 
+    jcomp \<lparr>jit_result := (jit_result jcomp)\<lparr>pc_section := x \<rparr>\<rparr>)"
+
+definition jit_emit :: "JitCompiler \<Rightarrow> u8 list  \<Rightarrow> JitCompiler" where
+"jit_emit l n = l
+ \<lparr>
+  jit_result              := (jit_result l)\<lparr> text_section := (text_section (jit_result l))@n \<rparr>,
+  offset_in_text_section  := (offset_in_text_section l) + of_nat (length n)
+ \<rparr>" *)
+
+abbreviation "REG_SCRATCH::ireg \<equiv> x64Syntax.R11"  
+
+definition bpf_to_x64_reg:: "bpf_ireg \<Rightarrow> ireg" where
+  "bpf_to_x64_reg br = (
+  case br of
+  BR0 \<Rightarrow> x64Syntax.RAX |
+  BR1 \<Rightarrow> x64Syntax.RDI |
+  BR2 \<Rightarrow> x64Syntax.RSI |
+  BR3 \<Rightarrow> x64Syntax.RDX |
+  BR4 \<Rightarrow> x64Syntax.RCX |
+  BR5 \<Rightarrow> x64Syntax.R8 |
+  BR6 \<Rightarrow> x64Syntax.RBX |
+  BR7 \<Rightarrow> x64Syntax.R13 |
+  BR8 \<Rightarrow> x64Syntax.R14 |
+  BR9 \<Rightarrow> x64Syntax.R15 |
+  BR10 \<Rightarrow> x64Syntax.RBP
+)"
+
+lemma bpf_to_x64_reg_corr2[simp]:" bpf_to_x64_reg r1 \<noteq> bpf_to_x64_reg r2  \<longrightarrow> r1 \<noteq> r2 "
+  apply(unfold bpf_to_x64_reg_def)
+  apply(rule impI)
+  apply(cases r1)
+    apply(cases r2, simp_all)
+           apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+         apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+       apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+  apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+  apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+  done
+
+lemma bpf_to_x64_reg_corr[simp]:" r1 \<noteq> r2 \<longrightarrow> bpf_to_x64_reg r1 \<noteq> bpf_to_x64_reg r2 "
+  apply(unfold bpf_to_x64_reg_def)
+  apply(rule impI)
+  apply(cases r1)
+    apply(cases r2, simp_all)
+           apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+         apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+       apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+  apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+  apply(cases r2, simp_all)
+    apply(cases r2, simp_all)
+  done
+
+definition per_jit_add_reg64_1 :: "bpf_ireg \<Rightarrow> bpf_ireg \<Rightarrow> x64_bin option" where
+"per_jit_add_reg64_1 dst src = (
+  let ins = Paddq_rr (bpf_to_x64_reg dst) (bpf_to_x64_reg src) in
+    x64_encode ins
+)"
+
+definition per_jit_exit :: "x64_bin option " where
+"per_jit_exit = (
+  let ins = Pret in
+    x64_encode ins
+)"
+
+(*
+datatype jit_state =
+  JIT_OK JitCompiler| (**r normal state *) (*regset SBPFV  mem Config*)
+  JIT_Success JitProgram|
+  JIT_EFlag | (**r find bugs at runtime *)
+  JIT_Err (**r bad thing *) *)
+
+fun per_jit_ins ::" bpf_instruction \<Rightarrow> x64_bin option"where
+"per_jit_ins bins = (
+  case bins of
+  BPF_ALU64 BPF_ADD dst (SOReg src) \<Rightarrow> (per_jit_add_reg64_1 dst src) |
+  BPF_EXIT \<Rightarrow> per_jit_exit |
+  _ \<Rightarrow> None
+)"
+
+(*
+fun per_jit_insns_aux ::"bpf_instruction list \<Rightarrow> x64_bin option list"where
+"per_jit_insns_aux []  = []" |
+"per_jit_insns_aux (x#xs) = (let val = per_jit_ins x in case val of None \<Rightarrow> [None] | _ \<Rightarrow> [val] @ per_jit_insns_aux xs )"
+
+definition per_jit_insns::"bpf_instruction list \<Rightarrow> x64_bin option" where
+"per_jit_insns l = (let l' = per_jit_insns_aux l in 
+                              if last l' = None then None else Some (concat(map Option.the (butlast l'))))"
+
+definition update_pc ::"JitCompiler \<Rightarrow> JitCompiler " where
+"update_pc jcomp = jcomp\<lparr> jit_pc := (jit_pc jcomp) + 1\<rparr>"
+
+definition jit_compile_aux::"u64 \<Rightarrow> bpf_instruction \<Rightarrow> JitCompiler \<Rightarrow> jit_state" where
+"jit_compile_aux pc bins jcomp = (let xins = per_jit_ins bins ; jprog = jit_result jcomp  in
+   if bins = BPF_EXIT then JIT_Success jprog else
+   (case xins of None \<Rightarrow> JIT_Err |
+                 Some v \<Rightarrow> let jcomp_updated1 = update_pc_section jcomp; jcomp_updated2 = jit_emit jcomp_updated1 v;
+                           jcomp_updated = update_pc jcomp_updated2 in 
+    JIT_OK jcomp_updated))"
+
+fun jit_compile :: "nat \<Rightarrow> bpf_bin \<Rightarrow> jit_state \<Rightarrow> jit_state" where
+"jit_compile 0 _ st =  JIT_EFlag " |
+"jit_compile (Suc fuel) prog st = (
+  case st of 
+  JIT_Err \<Rightarrow> JIT_Err |
+  JIT_Success jprog \<Rightarrow> JIT_Success jprog |
+  JIT_EFlag \<Rightarrow> JIT_EFlag |
+  JIT_OK jcomp \<Rightarrow> ( let pc = jit_pc jcomp in
+    if unat pc < length prog then
+        case bpf_find_instr (unat pc) prog of 
+          None \<Rightarrow> JIT_Err |
+          Some ins \<Rightarrow> jit_compile fuel prog (jit_compile_aux pc ins jcomp)      
+    else JIT_Err))"
+*)
+
+(**r 
+TODO: 
+1. from sbpf binary (interpreter.rs) to sbpf binary simplified: (0x18 lddw) to (0x18 lddw_low) + (0x10 lddw_high)
+2. from sbpf binary (simplified) to sbpf assembly
+*)
+
+fun jit1 :: "ebpf_asm \<Rightarrow> u8 list \<Rightarrow> usize list \<Rightarrow> (u8 list * usize list) option" where
+"jit1 [] l_bin l_pc = Some (l_bin, l_pc)" |
+"jit1 (h#t) l_bin l_pc = (
+  case per_jit_ins h of
+  None \<Rightarrow> None |
+  Some ins \<Rightarrow> (
+    jit1 t (l_bin @ ins) (l_pc @ [of_nat (length l_bin)])
+  )
+)"
+
+fun jit2 :: "ebpf_asm \<Rightarrow> (u8 list * usize list) option" where
+"jit2 [] = Some ([], [])" |
+"jit2 (h#t) = (
+  case jit2 t of
+  None \<Rightarrow> None |
+  Some (l_bin, l_pc) \<Rightarrow> (
+    case per_jit_ins h of
+    None \<Rightarrow> None |
+    Some ins \<Rightarrow> Some (ins @l_bin, 0#(map (\<lambda> i. i + (of_nat (length ins))) l_pc))
+  )
+)"
+
+(*
+fun jit :: "ebpf_asm \<Rightarrow> jit_state \<Rightarrow> jit_state option" where
+"jit [] st = Some st" |
+"jit (h#t) st = (
+  case per_jit_ins h of
+  None \<Rightarrow> None |
+  Some ins \<Rightarrow> jit t (upd_x64_ins ins st)
+)" *)
+
+(**r star of ebpf *)
+datatype sbpf_state =
+  SBPF_OK u64 reg_map mem stack_state SBPFV func_map u64 u64 | (**r normal state *)
+  SBPF_Success u64
+
+(*inductive sbpf_step :: "ebpf_asm * sbpf_state \<Rightarrow> ebpf_asm * sbpf_state \<Rightarrow> bool" (infix "\<rightarrow>s" 55)
+  where
+B_ADD64: "st = SBPF_OK pc rs m stk sv fm cur_cu remain_cu \<Longrightarrow>
+          BPF_ALU64 bop d sop = l!(unat pc) \<Longrightarrow>
+          bop = BPF_ADD \<Longrightarrow>
+          is_v1 = (case sv of V1 \<Rightarrow> True | _ \<Rightarrow> False) \<Longrightarrow>
+          OKS rs' = eval_alu32 bop d sop rs is_v1 \<Longrightarrow>
+          st' = SBPF_OK (pc+1) rs' m stk sv fm (cur_cu+1) remain_cu \<Longrightarrow>
+            (l, st) \<rightarrow>s (l, st')" |
+B_EXIT: " st = SBPF_OK pc rs m stk sv fm cur_cu remain_cu \<Longrightarrow>
+          BPF_EXIT = l!(unat pc) \<Longrightarrow>
+          call_depth stk = 0 \<Longrightarrow>
+            (l, st) \<rightarrow>s (l, SBPF_Success (rs BR0))"
+*)
+inductive sbpf_step :: "ebpf_asm * sbpf_state \<Rightarrow> ebpf_asm * sbpf_state \<Rightarrow> bool" (infix "\<rightarrow>s" 55)
+  where
+B_ADD64: "st = SBPF_OK pc rs m stk sv fm cur_cu remain_cu \<Longrightarrow>
+          BPF_ALU64 bop d sop = l!(unat pc) \<Longrightarrow>
+          bop = BPF_ADD \<Longrightarrow>
+          OKS rs' = eval_alu32 bop d sop rs is_v1 \<Longrightarrow>
+          st' = SBPF_OK (pc+1) rs' m stk sv fm (cur_cu+1) remain_cu \<Longrightarrow>
+            (l, st) \<rightarrow>s (l, st')" |
+B_EXIT: " st = SBPF_OK pc rs m stk sv fm cur_cu remain_cu \<Longrightarrow>
+          BPF_EXIT = l!(unat pc) \<Longrightarrow>
+            (l, st) \<rightarrow>s (l, SBPF_Success (rs BR0))"
+
+
+lemma success_is_final:"\<nexists> cs. (l,SBPF_Success n) \<rightarrow>s cs"
+  apply(induction n) apply auto 
+  using sbpf_step.cases apply force
+  using sbpf_step.cases by fastforce
+
+definition "final cs \<equiv> \<not>(\<exists> cs'. cs \<rightarrow>s cs')"
+
+lemma finalD:"s = SBPF_Success n \<Longrightarrow> final(l,s)"
+  using final_def success_is_final by auto
+
+(*lemma finalD2:" final cs \<Longrightarrow> cs = (l,s) \<Longrightarrow> s = SBPF_Success n"
+  apply(unfold final_def) apply(induction cs)*)
+
+
+abbreviation sbpf_sem::"ebpf_asm * sbpf_state \<Rightarrow> ebpf_asm * sbpf_state \<Rightarrow> bool" (infix "\<rightarrow>*s" 55)
+  where "x \<rightarrow>*s y == star sbpf_step x y"
+
+(*lemma success_is_final2:
+  assumes a0:"(l,st) \<rightarrow>s x" and
+  a1:"st = SBPF_OK 0 rs m stk sv fm cur_cu remain_cu" and 
+  a2:"l = [BPF_ALU64 BPF_ADD d sop]"
+shows" x = (l, SBPF_OK 1 rs' m stk sv fm (cur_cu+1) remain_cu)"*)
+lemma success_is_final2:"\<lbrakk> (l,st) \<rightarrow>s x;
+  st = SBPF_OK pc rs m stk sv fm cur_cu remain_cu ;
+  unat pc = 0 ;
+  l = [BPF_ALU64 BPF_ADD d sop] \<rbrakk>
+  \<Longrightarrow> x = (l, SBPF_OK (pc+1) rs' m stk sv fm (cur_cu+1) remain_cu)"
+proof (induction rule: sbpf_step.induct)
+  case (B_ADD64 st pc rs m stk sv fm cur_cu remain_cu bop d sop l rs' is_v1 st')
+  fix st pc rs m stk sv fm cur_cu remain_cu bop d sop l rs' is_v1 st'
+  have "unat pc = 0" 
+  then show ?case sorry
+next
+  case (B_EXIT st pc rs m stk sv fm cur_cu remain_cu l)
+  then show ?case apply(intro: small_steps.intros, simp_all)
+qed
+
+
+(*
+lemma success_is_final2:"(l,st) \<rightarrow>s x \<Longrightarrow>
+  st = SBPF_OK 0 rs m stk sv fm cur_cu remain_cu \<Longrightarrow> l = [BPF_ALU64 BPF_ADD d sop] 
+  \<Longrightarrow> x = (l, SBPF_OK 1 rs' m stk sv fm (cur_cu+1) remain_cu)"
+ apply blast
+  subgoal for a l*)
+
+
+lemma success_is_final2:"(l,st) \<rightarrow>*s (l,st') \<Longrightarrow>
+  st = SBPF_OK 0 rs m stk sv fm cur_cu remain_cu \<Longrightarrow> l = [BPF_ALU64 BPF_ADD d sop, BPF_EXIT] 
+  \<Longrightarrow> st' = SBPF_Success n"
+  apply simp_all
+  apply (drule star.step)
+   apply (rule star.refl)
+ 
+
+(**r star of x64 *)
+datatype x64_state =
+  Bin_OK u64 regset mem
+
+axiomatization x64_step :: "u8 list * x64_state \<Rightarrow> u8 list * x64_state \<Rightarrow> bool" (infix "\<rightarrow>x" 55)
+
+abbreviation x64_sem::"u8 list * x64_state \<Rightarrow> u8 list * x64_state \<Rightarrow> bool" (infix "\<rightarrow>*x" 55)
+  where "x \<rightarrow>*x y == star x64_step x y"
+
+(**r simulation relation *)
+definition match_state :: "sbpf_state \<Rightarrow> x64_state \<Rightarrow> usize list \<Rightarrow> bool" where
+"match_state bst xst pc_map = (
+  case bst of
+  SBPF_OK pc rs m stk sv fm cur_cu remain_cu \<Rightarrow> (
+    case xst of
+    Bin_OK xpc xrs xm \<Rightarrow>
+    (\<forall> r. Vlong (rs r) = xrs (IR (bpf_to_x64_reg r))) \<and> \<comment>\<open> for ALU + MEM + Call \<close>
+    pc_map!(unat pc) = xpc \<and>  \<comment>\<open> for Jump \<close>
+    m = xm  \<comment>\<open> for MEM + Call \<close>
+  ) |
+  SBPF_Success v \<Rightarrow>(
+    case xst of
+    Bin_OK xpc xrs xm \<Rightarrow> Vlong v = xrs (IR (bpf_to_x64_reg BR0)) \<comment>\<open> for EXIT \<close>
+  )
+)"
+
+
+theorem jit_correct:
+  assumes a0:"jit2 prog = Some (x64_prog, pc_map)" and
+    a1:"match_state bst xst pc_map" and
+    a2:"sbpf_sem (prog, bst) (prog, bst')"
+  shows "\<exists> xst'.
+      x64_sem (x64_prog, xst) (x64_prog, xst') \<and>
+      match_state bst' xst' pc_map"
+proof-
+  have "\<nexists> n. bst = SBPF_Success n" using success_is_final a2
+    by (meson sbpf_state.distinct(1) star.refl success_is_final2)
+  hence b1:"\<exists> pc rs m stk sv fm cur_cu remain_cu. bst = SBPF_OK pc rs m stk sv fm cur_cu remain_cu" 
+    by (meson sbpf_state.exhaust)
+  obtain pc rs m stk sv fm cur_cu remain_cu where b2:"bst = SBPF_OK pc rs m stk sv fm cur_cu remain_cu" using b1 by auto
+  have b3:"\<exists> xpc xrs xm. xst = Bin_OK xpc xrs xm"  by (meson x64_state.exhaust)
+  obtain xpc xrs xm where b4:"xst = Bin_OK xpc xrs xm" using b3 by auto
+  have b5:"(\<forall> r. Vlong (rs r) = xrs (IR (bpf_to_x64_reg r)))" using a1 b4 b2 by (simp add: match_state_def)
+  have b6:"\<exists> rs'. bst' = SBPF_OK (pc+of_nat (length prog)) rs' m stk sv fm (cur_cu+of_nat (length prog)) remain_cu" 
+   
+  sorry
+
+(*theorem jit_correct:
+  " jit2 prog = Some (x64_prog, pc_map) \<Longrightarrow>
+    match_state bst xst pc_map \<Longrightarrow>
+    sbpf_sem (prog, bst) (prog, bst') \<Longrightarrow>
+    (\<exists> xst'.
+      x64_sem (x64_prog, xst) (x64_prog, xst') \<and>
+      match_state bst' xst' pc_map)
+"
+  sorry
+*)
+
+fun interp3 :: "instruction list \<Rightarrow> outcome \<Rightarrow> outcome" where
+"interp3 [] s = s" |
+"interp3 (ins#l) st = (
+  case st of
+  Stuck \<Rightarrow> Stuck |
+  Next rs m \<Rightarrow> (
+        interp3 l (exec_instr ins 1 rs m)
+))"
+
+datatype bin_state =
+  Bin_OK u64 regset mem | (**r normal state *) (*reg_map mem stack_state Config SBPFV*)
+  Bin_Success|
+  Bin_EFlag | (**r find bugs at runtime *)
+  Bin_Err (**r bad thing *)
+
+definition getInsts::"JitProgram \<Rightarrow> instruction list option" where
+"getInsts jprog = (let insns = text_section jprog in 
+    case x64_decodes 0 insns of None \<Rightarrow> None |
+                                Some v \<Rightarrow> Some (map snd v)) "
+
+fun binary_execute :: "nat \<Rightarrow> JitProgram \<Rightarrow> bin_state \<Rightarrow> bin_state" where
+"binary_execute 0 _ st =  Bin_EFlag " |
+"binary_execute (Suc fuel) jprog st = (
+  case st of 
+  Bin_Err \<Rightarrow> Bin_Err |
+  Bin_Success \<Rightarrow> Bin_Success |
+  Bin_EFlag \<Rightarrow> Bin_EFlag |
+  Bin_OK pc rs m \<Rightarrow> (let v = getInsts jprog in 
+    case v of None \<Rightarrow> Bin_Err |
+              Some insns \<Rightarrow> 
+    if unat pc < length insns then
+      let result = interp3 insns (Next rs m) in 
+        case result of Stuck \<Rightarrow> Bin_EFlag | Next rs' m' \<Rightarrow> Bin_OK (pc+ of_nat(length insns)) rs' m'
+    else Bin_Err))"
+
+
+definition init_jitprog::"JitProgram" where
+"init_jitprog \<equiv> \<lparr> page_size = 4096, pc_section = [], text_section = []\<rparr>"
+
+(*
+definition init_jitcomp::"JitCompiler" where
+"init_jitcomp = \<lparr> jit_result = init_jitprog, offset_in_text_section = 0, jit_pc = 0\<rparr>" *)
+ 
+end
