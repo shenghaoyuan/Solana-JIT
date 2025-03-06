635cedabc31f8d1587fb257e96678028650927c9
shenghaoyuan <shenghaoyuan0928@163.com>
2025-03-06 17:21:19 +0800

    add: per_mul_rax

---
+++ theory/JITPer_mul_rax.thy
@@ -0,0 +1,23 @@
+theory JITPer_mul_rax
+imports
+  Main
+  rBPFCommType rBPFSyntax rBPFSem
+  x64Syntax x64Semantics x64Assembler
+  JITPer_aux
+
+begin
+
+lemma mulq_one_step_rax:
+assumes a0:"s' = sbpf_step prog s" and
+  a1:"s = (SBPF_OK pc rs m)" and
+  a2:"s' = (SBPF_OK pc' rs' m')" and
+  a3:"xst = (Next xpc xrs xm)" and
+  a4:"match_state s (pc,xst)" and
+  a5:"jitper prog = Some x64_prog" and                      
+  a6:"prog \<noteq> [] \<and> unat pc < length prog \<and> unat pc \<ge> 0" and
+  a8:"prog!(unat pc) = BPF_ALU64 BPF_MUL dst (SOReg src)" and
+  a9:"(bpf_to_x64_reg dst) = RAX "
+shows "\<exists> xst'. x64_sem1 1 x64_prog (pc,xst) = (pc',xst') \<and> 
+  match_state s' (pc',xst')"
+  sorry
+end
